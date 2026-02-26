11803df968a2c8e20f487c2539835d4a9125749c
Gabriel Alencar <117116076+devalenca@users.noreply.github.com>
2026-02-26 10:31:07 -0300

    refactor: clean up DevelopersService by removing unnecessary return statements

--- src/developers/developers.service.ts
+++ src/developers/developers.service.ts
@@ -9,28 +9,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class DevelopersService {
   constructor(
     @InjectRepository(Developer)
-    private readonly repository: Repository<Developer>
-  ) {}
+    private readonly repository: Repository<Developer>,
+  ) { }
 
   create(createDeveloperDto: CreateDeveloperDto) {
     const developer = this.repository.create(createDeveloperDto);
     return this.repository.save(developer);
-    return 'This action adds a new developer';
   }
 
   findAll() {
     return this.repository.find();
-    return `This action returns all developers`;
   }
 
   findOne(id: string) {
     return this.repository.findOneBy({ id });
-
-    return `This action returns a #${id} developer`;
   }
 
   async update(id: string, updateDeveloperDto: UpdateDeveloperDto) {
-    const developer = await this.repository.findOneBy({ id }); 
+    const developer = await this.repository.findOneBy({ id });
     if (!developer) {
       return null;
     }
